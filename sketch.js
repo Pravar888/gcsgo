0c1a9e7a330926ad35daffd67a913cfec6405b79
Pravar888 <64853677+Pravar888@users.noreply.github.com>
2020-06-09 19:38:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var call,wall;
+var speed ,weight = random(400,1500);
+
+
+
+
+function setup() {
+  createCanvas(1600,400);
+  car = createSprite(50,200,50,50);
+  wall = createSprite(1500,200,60,height/2);
+  speed = random(55,90);
+  weight = random(400,1500);
+  car.velocityX = speed;
+
+}
+
+function draw() {
+  background(255,255,255);  
+ 
+ 
+  drawSprites();
+}
